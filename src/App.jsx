4707b4bafec5dfc8d32d0ee6013907c4a1dd82d0
James09977 <zakaria.james1@gmail.com>
2024-11-25 22:11:18 +0600

    nav

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import DaisyNav from "./assets/Components/DaisyNav/DaisyNav";
+import NavBar from "./assets/Components/DaisyNav/NavBar/NavBar";
+
+function App() {
+  return (
+    <>
+      {/* <DaisyNav></DaisyNav> */}
+      <NavBar></NavBar>
+    </>
+  );
+}
+
+export default App;
